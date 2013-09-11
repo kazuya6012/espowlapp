91e51d91eed1a721a048f57e0b990ac2a92e29b2
kazuya6012 <www.jam.zbz@gmail.com>
2013-09-11 21:17:02 +0900

    Hello

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Espowlapp::Application.config.secret_key_base = '7cf078c45e6879a800a3417e0d48f59a4e295800a032256c62432f5d7c58113176a4ad8ea6ddfdb096fd09bc5f67dc07aa97775e9dbf16ec5fd7babcce13e70e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Espowlapp::Application.config.session_store :cookie_store, key: '_espowlapp_session'
